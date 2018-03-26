48f17eb8e85f4dda68ce581095be937978188425
Liam Dillingham <mailgd9@gmail.com>
2018-03-26 15:33:29 -0400

    basic framework

---
+++ LayerConv.java
@@ -0,0 +1,21 @@
+
+
+
+class LayerConv extends Layer {
+
+
+  LayerConv() {
+
+  }
+
+  LayerConv(int[] inputDims, int[] filterDims, int[] outputDims) {
+
+  }
+
+  /// Instantiate like:
+  // nn.add(new LayerConv(new int[]{28, 28},
+  //   new int[]{5, 5, 32},
+  //   new int[]{28, 28, 32}));
+
+
+}
